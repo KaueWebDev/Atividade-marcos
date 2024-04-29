e0a0f8bfba536dbbfda135c3a47232c6fa466f1b
Kauê Anderson <164877319+KaueWebDev@users.noreply.github.com>
2024-04-29 09:29:34 -0300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 print("Oi, eu me chamo Kauê Anderson")
-calc = input("Digite seu nome para calcular a conta já prgramada:")
+inicio = input("Digite seu nome para calcular a conta já prgramada:")
 aleat1 = random.randint(0, 100)
 aleat2 = random.randint(0, 100)
 x = aleat1
