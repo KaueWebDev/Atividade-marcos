ac8b947814a10bfe07778470ec647c7307f43a04
Kauê Anderson <164877319+KaueWebDev@users.noreply.github.com>
2024-04-29 09:28:28 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+print("Oi, eu me chamo Kauê Anderson")
+calc = input("Digite seu nome para calcular a conta já prgramada:")
+aleat1 = random.randint(0, 100)
+aleat2 = random.randint(0, 100)
+x = aleat1
+y = aleat2
+soma = x + y
+print(f"Kauê somou {soma}")
